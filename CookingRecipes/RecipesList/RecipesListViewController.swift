af3a1c131e8cef782da0683d29858d98d49390e6
Dima Protsak <protsack.dima25@gmail.com>
2023-04-11 14:40:30 +0300

    feat: added tableview and cell's

--- CookingRecipes/RecipesList/RecipesListViewController.swift
+++ CookingRecipes/RecipesList/RecipesListViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class RecipesListViewController: UIViewController {
-
+    
+    private let tableView : UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    var receipts: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
         
+        setupTableView()
     }
-
-
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
 }
 
+extension RecipesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return receipts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+extension RecipesListViewController: UITableViewDelegate {
+    
+}
