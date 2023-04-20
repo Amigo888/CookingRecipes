791dc086a90f8c574dd2549ec37d3a6ffb0fe2a4
Dima Protsak <protsack.dima25@gmail.com>
2023-04-20 17:30:55 +0300

    feat: configure and setup IngridientCell

---
+++ CookingRecipes/RecipesListDetailed/UI/CollectionViewCell/IngridientCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IngridientCollectionViewCell.swift
+//  CookingRecipes
+//
+//  Created by Дмитрий Процак on 20.04.2023.
+//
+
+import UIKit
+
+class IngridientCollectionViewCell: UICollectionViewCell {
+    
+}
